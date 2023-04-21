a8d3a2b3d4a41a1aba21769ffaac2b4ee2009ba0
Cameron Chandler <webstormcam@gmail.com>
2023-04-21 19:15:06 -0400

    Got the movies to all appear in the console
    They have all their information!!!!!

--- js/script.js
+++ js/script.js
@@ -8,7 +8,12 @@ fetch('https://www.omdbapi.com/?apikey=55ea45d4&s=blade runner')
 .then(res=>res.json())
 .then(data =>{
     for(let i=0;i<data.Search.length;i++){
-        console.log(data.Search[i].imdbID)
+        fetch(`https://www.omdbapi.com/?apikey=55ea45d4&i=${data.Search[i].imdbID}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+        
     }
     
 })
