fec9b309da7f5864edbf58d2d8a9fad0cc85fd17
Cameron Chandler <webstormcam@gmail.com>
2023-04-21 18:55:49 -0400

    Building out the js to access the movies

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const searchMovie = document.getElementById('search')
 searchMovie.addEventListener('click',function(){
     console.log('clicked')
 })
+
+fetch('https://www.omdbapi.com/?apikey=55ea45d4&s=blade runner')
+.then(res=>res.json())
+.then(data =>{
+    for(let i=0;i<data.Search.length;i++){
+        console.log(data.Search[i].imdbID)
+    }
+    
+})
