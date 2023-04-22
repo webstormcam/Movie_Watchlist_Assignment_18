7d3e0be838c04151ca13a6b0da7a87b8766e596f
Cameron Chandler <webstormcam@gmail.com>
2023-04-22 09:51:59 -0400

    Wrote in some negative response
    basically made it so that if the values return with nothing to loop through it does not allow it to move forward and display movies

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 const searchMovie = document.getElementById('search')
 let inputValue = document.getElementById('enter-movie')
+const placeholderArea = document.getElementById('placeholder-place')
 searchMovie.addEventListener('click',function(){
     let searchedValue = inputValue.value
-    fetch(`https://www.omdbapi.com/?apikey=55ea45d4&s=${searchedValue}`)
-    .then(res=>res.json())
-    .then(data =>{
-        for(let i=0;i<data.Search.length;i++){
-            fetch(`https://www.omdbapi.com/?apikey=55ea45d4&i=${data.Search[i].imdbID}`)
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data)
-            })
+   
+        fetch(`https://www.omdbapi.com/?apikey=55ea45d4&s=${searchedValue}`)
+        .then(res=>res.json())
+       
+        .then(data =>{
+            if(data.Response==="False"){
+                console.log('NAH no way bro')
+            } else{
+                for(let i=0;i<data.Search.length;i++){
+                    fetch(`https://www.omdbapi.com/?apikey=55ea45d4&t=${data.Search[i].Title}`)
+                    .then(res=>res.json())
+                    .then(data=>{
+                        console.log(data)
+                    })
+                    
+                }
+            }
+           
             
-        }
-        
-    })
+        })
+    
+   
 })
 
