cfd302e9aae2a6221862b25709e73cf6c1dfdb48
Cameron Chandler <webstormcam@gmail.com>
2023-04-22 06:51:27 -0400

    Now the input value is taken. :)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 const searchMovie = document.getElementById('search')
-
+let inputValue = document.getElementById('enter-movie')
 searchMovie.addEventListener('click',function(){
-    console.log('clicked')
+    let searchedValue = inputValue.value
+    fetch(`https://www.omdbapi.com/?apikey=55ea45d4&s=${searchedValue}`)
+    .then(res=>res.json())
+    .then(data =>{
+        for(let i=0;i<data.Search.length;i++){
+            fetch(`https://www.omdbapi.com/?apikey=55ea45d4&i=${data.Search[i].imdbID}`)
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+            })
+            
+        }
+        
+    })
 })
 
-fetch('https://www.omdbapi.com/?apikey=55ea45d4&s=blade runner')
-.then(res=>res.json())
-.then(data =>{
-    for(let i=0;i<data.Search.length;i++){
-        fetch(`https://www.omdbapi.com/?apikey=55ea45d4&i=${data.Search[i].imdbID}`)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data)
-        })
-        
-    }
-    
-})
