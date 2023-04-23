f754579a45bcbe33a7150aec0ed321e53805bb1d
Cameron Chandler <webstormcam@gmail.com>
2023-04-23 08:03:54 -0400

    Placed in the search error screen

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ searchMovie.addEventListener('click',function(){
        
         .then(data =>{
             if(data.Response==="False"){
-                console.log('NAH no way bro')
+                placeholderArea.innerHTML =`
+                <p class="unable-text">Unable to find what you're looking for. Please try another search.</p>`
             } else{
                 for(let i=0;i<data.Search.length;i++){
                     fetch(`https://www.omdbapi.com/?apikey=55ea45d4&t=${data.Search[i].Title}`)
