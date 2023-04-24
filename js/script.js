6264e66dc2af03e4e3a300856fe622f0623a02e1
Cameron Chandler <webstormcam@gmail.com>
2023-04-23 20:33:16 -0400

    Changed to async Fetch
    Code loads one line at a time and is ready to be styled on request

--- js/script.js
+++ js/script.js
@@ -1,43 +1,32 @@
 const searchMovie = document.getElementById('search')
 let inputValue = document.getElementById('enter-movie')
-const dataDisplay = document.getElementById('data-display')
-let arrayOfFilms=[];
-let movies =""
+let dataDisplay = document.getElementById('data-display')
+let grouptitles =``
 searchMovie.addEventListener('click',function(){
-    arrayOfFilms =[];
     let searchedValue = inputValue.value
     inputValue.value =""
-   
-        fetch(`https://www.omdbapi.com/?apikey=55ea45d4&type=movie&s=${searchedValue}`)
-        .then(res=>res.json())
-       
-        .then(data =>{
-            if(data.Response==="False"){
-                dataDisplay.innerHTML =`
+   fetchMoviesJSON(searchedValue)    
+})
+
+
+async function fetchMoviesJSON(searchableMovie){
+    const res = await fetch(`https://www.omdbapi.com/?apikey=55ea45d4&type=movie&s=${searchableMovie}`)
+    const movies = await res.json()
+    if(movies.Response==="False"){
+        dataDisplay.innerHTML =`
                 <div id="placeholder-place" class="place-holder-area">
                 <p class="unable-text">Unable to find what you're looking for. Please try another search.</p>
                 </div>`
-            } else{
-                for(let i=0;i<data.Search.length;i++){
-                    fetch(`https://www.omdbapi.com/?apikey=55ea45d4&t=${data.Search[i].Title}`)
-                    .then(res=>res.json())
-                    .then(data=>{
-                         movies+= `<div>
-                        <h2>${data.Title}</h2>
-                        </div>`
-            
-                    })
-                    
-                }
-                dataDisplay.innerHTML = movies
-              
-            }
+    } else{
+        grouptitles= ``
+        for(let i = 0; i<movies.Search.length;i++){
+           const res = await fetch(`https://www.omdbapi.com/?apikey=55ea45d4&t=${movies.Search[i].Title}`)
+           const fullMovie = await res.json()
+           grouptitles+= `<div><h2>${fullMovie.Title}</h2></div>`
+        }
+        console.log(grouptitles)
+       dataDisplay.innerHTML=grouptitles
+    }
 
-          
-           
-            
-        })
-    
-  
-})
+}
 
